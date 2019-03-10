5e15aedda623b1bbf7432d4df41116ce952ad487
Keshav Garg <keshav.garg@team.telstra.com>
2019-03-11 04:38:22 +1100

    Added Prometheus with Default Metrics Monitoring

--- src/app.js
+++ src/app.js
@@ -2,20 +2,41 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const appRoutes = require('./app-routes');
+const Prometheus = require('./shared/utils/prometheus');  
 
 const port = process.env.PORT || 3000;
 
-
+// creating the express app
 const app = express();
 
+/***** Configuring the application *****/
 app.use(bodyParser.json());
 app.use(
   bodyParser.urlencoded({
     extended: true
   })
 );
+// registering the app routes
 app.use(appRoutes);
 
+
+/**
+ * The below arguments start the counter functions
+ */
+app.use(Prometheus.requestCounters);  
+app.use(Prometheus.responseCounters);
+
+/**
+ * Enable metrics endpoint
+ */
+Prometheus.injectMetricsRoute(app);
+
+/**
+ * Enable collection of default metrics
+ */
+Prometheus.startCollection(); 
+
+/***** App configuration ends *****/
 const bootstrapApp = app => {
   app.listen(port, () => {
     console.log(`App running on port: ${port}`);
@@ -23,58 +44,6 @@ const bootstrapApp = app => {
 };
 
 
-
 bootstrapApp(app);
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const express = require("express");
-// const bodyParser = require("body-parser");
-
-// const { errorHandler } = require("./shared/utils");
-// const appRoutes = require("./app-routes");
-
-// const port = process.env.PORT || 3000;
-
-// // creating the express app
-// const app = express();
-
-// /***** Configuring the application *****/
-
-// // Body-parser
-// app.use(bodyParser.json());
-// app.use(
-//   bodyParser.urlencoded({
-//     extended: true
-//   })
-// );
-
-// // registering the app routes
-// app.use(appRoutes);
-
-// // register error handler
-// app.use(errorHandler);
-
-// /***** App configuration ends *****/
-
-// const bootstrapApp = app => {
-//   app.listen(port, () => {
-//     console.log(`App working on ${port}`);
-//   });
-// };
-
-// bootstrapApp(app);
